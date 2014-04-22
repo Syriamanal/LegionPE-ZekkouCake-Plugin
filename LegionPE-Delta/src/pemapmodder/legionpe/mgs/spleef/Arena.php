50af7652f0435c74a33e1c3d2045845913877815
mcpesrccode <pemapmodder1970@gmail.com>
2014-04-23 02:06:07 +0800

    Started spleef work

---
+++ LegionPE-Delta/src/pemapmodder/legionpe/mgs/spleef/Arena.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace pemapmodder\legionpe\mgs\spleef;
+
+class Arena{
+	public function __construct(Position $topCentre, $radius,
+			$height, $floors, $players,
+			Block $floor, Block $pfloor, Block $pwall, Block $pceil){
+		$this->centre = $topCentre;
+		$this->radius = $radius;
+		$this->height = $height;
+		$this->floors = $floors;
+		$this->gfloor = $floor;
+		$this->pfloor = $pfloor;
+		$this->pwall = $pwall;
+		$this->pceil = $pceil;
+		$this->status = 0;
+		$this->players = array();
+		$this->build($players);
+	}
+	protected function build($cnt){
+		$this->preps = Builder::build($this->centre, $this->radius, $this->gfloor, $this->floors, $this->height, $cnt, $this->pfloor, $this->pwall, $this->pceil);
+	}
+	public function join(Player $join){
+		if($this->status === 1 or count($this->players) >= count($this->preps) or isset($this->players[$join->getCID(}]))
+			return false;
+		$this->players[$join->getCID()] = $join;
+		$join->teleport($this->preps[count($this->players) - 1]->add(0.5, 0.5, 0.5));
+	}
+	public function onInteract($event){
+	}
+	public function onMove($event){
+	}
+}
---
+++ LegionPE-Delta/src/pemapmodder/legionpe/mgs/spleef/Main.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace pemapmodder\legionpe\mgs\spleef;
+
+class Main{
+	public $arenas = array();
+}
