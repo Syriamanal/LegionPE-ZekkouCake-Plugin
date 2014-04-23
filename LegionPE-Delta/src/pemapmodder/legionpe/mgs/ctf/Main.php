8f81dcd53a506e016372e8657939ee37db3b6c30
mcpesrccode <pemapmodder1970@gmail.com>
2014-04-23 14:37:23 +0800

    I did not plan to start CTF so soon...

--- LegionPE-Delta/src/pemapmodder/legionpe/mgs/ctf/Main.php
+++ LegionPE-Delta/src/pemapmodder/legionpe/mgs/ctf/Main.php
@@ -2,8 +2,16 @@
 
 namespace pemapmodder\legionpe\mgs\ctf;
 
+use pemapmodder\legionpe\hub\HubPlugin;
+
+use pemapmodder\utils\FileUtils;
+
+use pocketmine\Server;
+
 class Main{
 	public function __construct(){
+		$this->hub = HubPlugin::get();
+		$this->server = Server::getInstance();
 	}
 	public static $ctf = false;
 	public static function get(){
