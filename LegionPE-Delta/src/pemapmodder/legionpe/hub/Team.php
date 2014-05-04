c841e55dea042789c48b17f443b8cc03a6c291e4
mcpesrccode <pemapmodder1970@gmail.com>
2014-05-04 12:48:50 +0800

    Untitled commit

--- LegionPE-Delta/src/pemapmodder/legionpe/hub/Team.php
+++ LegionPE-Delta/src/pemapmodder/legionpe/hub/Team.php
@@ -10,9 +10,9 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\tile\Tile;
 
-class Team implements \ArrayAccess{
+class Team{
 	// static
-	public static $teams = array();
+	protected static $teams = array();
 	public static function get($i){
 		if(is_int($i)) $i &= 0b11;
 		elseif($i instanceof Player) $i = HubPlugin::get()->getDb($i)->get("team");
@@ -28,12 +28,18 @@ class Team implements \ArrayAccess{
 			self::$teams[$i] = new self($i);
 		Server::getInstance()->getScheduler()->scheduleRepeatingTask(new CallbackPluginTask(array(get_class(), "updateScoreBars"), HubPlugin::get()), 600);
 	}
+	public static function dumpTeams($t){
+		if(!(self::$teams[0] instanceof self)){
+			console("At tick $t team 0 not Team instance");
+			var_dump(self::$teams[0]);
+		}
+	}
 	public static function canJoin($team){
 		$scores = array();
 		foreach(self::$teams as $t){
-			$scores[$t->getTeam()] = $t["members-cnt"];
+			$scores[$t->getTeam()] = $t->config["members-cnt"];
 		}
-		$ts = self::$teams[$team]["members-cnt"];
+		$ts = self::$teams[$team]->config["members-cnt"];
 		$max = max($scores);
 		$percent = ($max - $ts) / $ts * 100;
 		return $ts <= 5;
@@ -41,26 +47,22 @@ class Team implements \ArrayAccess{
 	public static function updateSigns(){
 		for($i = 0; $i < 4; $i++){
 			if(self::canJoin($i)){
-				DummyPlugin::getTile(RL::chooseTeamSigns($i))->setText("Tap me to join", "TEAM ".strtoupper(self::$teams[$i]["name"]));
+				DummyPlugin::getTile(RL::chooseTeamSigns($i))->setText("Tap me to join", "TEAM ".strtoupper(self::$teams[$i]->config["name"]));
 			}
 			else{
-				DummyPlugin::getTile(RL::chooseTeamSigns($i))->setText("TEAM ".strtoupper(self::$teams[$i]["name"]), "is now full.", "Come back later", "or join others");
+				DummyPlugin::getTile(RL::chooseTeamSigns($i))->setText("TEAM ".strtoupper(self::$teams[$i]->config["name"]), "is now full.", "Come back later", "or join others");
 			}
 		}
 	}
 	public static function updateScoreBars(){
 		$scores = array();
 		for($i = 0; $i < 4; $i++){
-			if(!(self::$teams[$i] instanceof self)){
-				console("[WARNING] hub\\Team::\$teams[$i] is not instanceof Team!");
-				return;
-			}
-			$scores[$i] = self::$teams[$i]["points"];
+			$scores[$i] = self::$teams[$i]->config["points"];
 		}
 		$max = max($scores);
 		for($i = 0; $i < 4; $i++){
 			$percent = max(0, $scores[$i]) / $max * 100;
-			RL::teamScoreBar($i, $percent)->setBlocks(Block::get(35, self::$teams[$i]["color-meta"]));
+			RL::teamScoreBar($i, $percent)->setBlocks(Block::get(35, self::$teams[$i]->config["color-meta"]));
 		}
 		console("[INFO] Hub score bars have been updated.");
 	}
@@ -70,8 +72,9 @@ class Team implements \ArrayAccess{
 		$this->team = $i;
 		$path = \pocketmine\DATA."hub/teams/team-$i.yml";
 		$this->path = $path;
+		Server::getInstance()->getScheduler()->scheduleRepeatingTask(new CallbackPluginTask(array($this, "save"), HubPlugin::get()), 1200);
 		if(is_file($path)){
-			$this->config = \yaml_parse(\file_get_contents($path));
+			$this->config = yaml_parse(file_get_contents($path));
 		}
 		else{
 			$names = array("magma", "lapiz", "lilac", "lime");
@@ -80,11 +83,11 @@ class Team implements \ArrayAccess{
 			$this->config["color-meta"] = $metas[$i];
 			$this->config["points"] = 1000;
 			$this->config["members-cnt"] = 10;
-			\file_put_contents($path, \yaml_emit($this->config));
+			file_put_contents($path, yaml_emit($this->config));
 		}
 	}
 	public function save(){
-		\file_put_contents($this->path, \yaml_emit($this->config));
+		file_put_contents($this->path, yaml_emit($this->config));
 	}
 	public function join(Player $p){
 		if(self::canJoin($this->team)){
